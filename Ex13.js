84f1882b87fe98212eaf2b77f85590c826a0203f
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-13 01:27:32 +0500

    Add files via upload

---
+++ Ex13.js
@@ -0,0 +1,8 @@
+"use strict";
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var trans = ["Motorbike", "Bullet Trains", "Rocket", "Submarine"];
+var list = ["Honda 125 is my favouriate ", "People in Janap frequently use ", "Mode of transport to travel to moon is ", "Recently a few people lost their lives in "];
+console.log(list[0] + trans[0]);
+console.log(list[1] + trans[1]);
+console.log(list[2] + trans[2]);
+console.log(list[3] + trans[3]);
